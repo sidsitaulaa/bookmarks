4161e76a796166da3dae4a96675ac858bdbabc9d
Sid <slothxsteroids@gmail.com>
2024-03-06 23:10:31 +0545

    [ADD] added image app

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social_django',
-    'django_extensions'
+    'django_extensions',
+    'images.apps.ImagesConfig'
 ]
 
 MIDDLEWARE = [
---
+++ bookmarks/images/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import ImageCreateForm
+
+# Create your views here.
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+
+        form = ImageCreateForm(data=request.POST)
+        
+        if form.is_valid():
+
+            cd = form.cleaned_data
+            new_image = form.save(commit=False)
+            new_image.user = request.user
+            print(request.user)
+            new_image.save()
+            print('==>',new_image)
+            messages.success(request,'Image added successfully')
+            return redirect('images:create')
+        
+
+    else:
+        form = ImageCreateForm(data=request.GET)
+    return render(request,'images/image/create.html',{'section':'images','form':form})
